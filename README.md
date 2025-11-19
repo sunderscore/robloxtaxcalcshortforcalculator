6c594d60b7a92348b8a7fbfd5ab5afddeaa1e2ca
superfluffypawss <ashtontoner555@gmail.com>
2025-11-19 18:07:28 +0000

    Website Upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# robloxtaxcalcshortforcalculator
-Roblox / PLS Donate calculator for lazy asses like myself
+# Roblox / PLS Donate Tax Calculator
+Roblox / PLS Donate Tax Calcultor made just so I don't have to open the calculator.
+Hope this can help others too though! :D
+
+Fully coded by @sunderscore (@superfluffypawss on roblox)!
---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'roblox-calc-v1';
+const ASSETS = [
+  'index.html',
+  'about.html',
+  'privacy.html',
+  'manifest.json',
+  'favicon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : Promise.resolve())))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    fetch(req)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req))
+  );
+});
